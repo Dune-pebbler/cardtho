5a34537d138eeb20032a79f9fbaef31f5d116120
Ruben Boon <Ruben95bo@gmail.com>
2025-08-21 11:17:45 +0200

    worked on responsivebness

--- single-product.php
+++ single-product.php
@@ -7,8 +7,8 @@ $gallery_images = get_field('product_afbeeldingen');
 
 <section class="single-product">
     <div class="product-container">
-        <div class="container arrow-bg ">
-            <div class="row">
+        <div class="container arrow-bg">
+            <div class="row align-items-start">
                 <div class="arrow-1">
                 </div>
                 <div class="arrow-2" data-animate="slide-in-right" data-animate-delay="300">
@@ -16,13 +16,13 @@ $gallery_images = get_field('product_afbeeldingen');
                 <div class="arrow-3" data-animate="slide-in-right" data-animate-delay="300">
                 </div>
                 <?php if ($gallery_images): ?>
-                    <div class="col-12 col-lg-6">
+                    <div class="col-12 col-lg-6 order-1 order-lg-1">
                         <div class="main-image">
                             <div class="owl-carousel owl-1 product-image-slider">
                                 <?php foreach ($gallery_images as $image): ?>
                                     <div class="item">
                                         <a href="<?= esc_url($image['url']); ?>" data-fancybox="product-gallery">
-                                            <img src="<?= esc_url($image['url']); ?>" alt="<?= esc_attr($image['alt']); ?>" />
+                                            <img src="<?= esc_url($image['url']); ?>" alt="<?= esc_attr($image['alt']); ?>" loading="lazy" />
                                         </a>
                                     </div>
                                 <?php endforeach; ?>
@@ -31,22 +31,23 @@ $gallery_images = get_field('product_afbeeldingen');
                         <div class="product-thumbnail-slider owl-carousel owl-theme mt-3">
                             <?php foreach ($gallery_images as $image): ?>
                                 <div class="item">
-                                    <img src="<?= esc_url($image['sizes']['thumbnail']); ?>" alt="<?= esc_attr($image['alt']); ?>" />
+                                    <img src="<?= esc_url($image['sizes']['thumbnail']); ?>" alt="<?= esc_attr($image['alt']); ?>" loading="lazy" />
                                 </div>
                             <?php endforeach; ?>
                         </div>
                     </div>
                 <?php endif; ?>
-                <div class="col-lg-1"></div>
-                <div class="col-12 col-lg-5">
-                    <?= the_content(); ?>
-                    <div class="product-buttons">
-                        <a href="https://cardthostore.nl/product/<?= get_post_field('post_name') ?>" target="_blank" class="btn">Koop in shop</a>
-                        <?php if ($anchor_link): ?>
-                            <a href="#anchor" class="btn btn-secondary"><?= esc_html($anchor_link); ?></a>
-                        <?php endif; ?>
+                <div class="col-lg-1 d-none d-lg-block"></div>
+                <div class="col-12 col-lg-5 order-2 order-lg-2">
+                    <div class="product-content">
+                        <?= the_content(); ?>
+                        <div class="product-buttons">
+                            <a href="https://cardthostore.nl/product/<?= get_post_field('post_name') ?>" target="_blank" class="btn btn-primary">Koop in shop</a>
+                            <?php if ($anchor_link): ?>
+                                <a href="#anchor" class="btn btn-secondary"><?= esc_html($anchor_link); ?></a>
+                            <?php endif; ?>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </div>
